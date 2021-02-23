0dfb5ca6185220887e689fbcc839140fd887e3da
Antoine Coulon <antoine@30c27b.com>
2021-02-23 11:58:45 +0100

    corrected header error

--- include/carbon.h
+++ include/carbon.h
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   carbon.h                                             :+:      :+:    :+:   */
+/*   carbon.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 13:41:06 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/02/06 16:33:32 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/02/23 11:58:31 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CARBON_H
-# define CARBON_H
+#define CARBON_H
 
-# include "carbon/mem.h"
-# include "carbon/str.h"
-# include "carbon/llst.h"
-# include "carbon/fmt.h"
+#include "carbon/mem.h"
+#include "carbon/str.h"
+#include "carbon/llst.h"
+#include "carbon/fmt.h"
 
 #endif
