62b4e61fc8d8f213f83dd5e053d2eb194b8db6f6
Antoine Coulon <antoine@30c27b.com>
2021-03-07 12:05:41 +0100

    corrected llst_pop

--- source/llst/lst_pop.c
+++ source/llst/lst_pop.c
@@ -6,22 +6,30 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 11:54:23 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/06 16:45:54 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/03/07 11:08:11 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "carbon/llst.h"
+#include <stdio.h>
 
 t_llst*
 	llst_pop(t_llst **lst)
 {
+	t_llst	*node;
 	t_llst	*tail;
 	
 	if (!lst || !*lst)
 		return (NULL);
-	while (*lst && (*lst)->next && (*lst)->next->next)
-		*lst = (*lst)->next;
-	tail = (*lst)->next;
-	(*lst)->next = NULL;
+	node = *lst;
+	if (!node->next)
+	{
+		*lst = NULL;
+		return (node);
+	}
+	while (node && node->next && node->next->next)
+		node = node->next;
+	tail = node->next;
+	node->next = NULL;
 	return (tail);
 }
