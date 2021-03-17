e1a6e64aca77171198429233eed6c5b27b38f8b0
Antoine Coulon <antoine@30c27b.com>
2021-03-17 15:33:30 +0100

    added llst_len

--- include/carbon/llst.h
+++ include/carbon/llst.h
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 19:33:09 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/02/25 12:25:16 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/03/17 15:33:17 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ typedef struct	s_llst
 */
 t_llst	*llst_new(void *data);
 
+size_t	llst_len(t_llst *node);
+
 t_llst	*llst_tail(t_llst *node);
 
 void	llst_push(t_llst **lst, t_llst *node);
@@ -52,6 +54,5 @@ t_llst	*llst_shift(t_llst **lst);
 void	llst_destroy(t_llst *node, void (*del)(void *));
 
 void	llst_destroyl(t_llst **lst, void (*del)(void *));
-
 
 #endif
