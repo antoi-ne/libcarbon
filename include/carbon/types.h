9b9da76b04465407f8bbcec2477926c09decbd8c
ancoulon <ancoulon@student.s19.be>
2021-05-28 13:26:54 +0200

    norm error

--- include/carbon/types.h
+++ include/carbon/types.h
@@ -6,15 +6,15 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 10:48:41 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/05/28 13:20:40 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/05/28 13:26:37 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
-#include <stdint.h>
+# include <stdint.h>
 
-int64_t		types_str2int(char *s);
+int64_t	types_str2int(char *s);
 
 #endif
